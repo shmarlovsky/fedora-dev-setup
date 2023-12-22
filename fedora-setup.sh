49c814fcd6f360ed847014ef198d6eda66460df6
kiryl.shmarlouski <kiril@shmarlovski.by>
2023-12-22 14:48:14 +0200

    uncomment steps

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "Fedora auto setup script"
 
 mygit="https://github.com/shmarlovsky"
@@ -18,29 +17,32 @@ echo "Alacritty dir: $alacritty_config"
 echo "Enter sudo password (needed for installing packages):"
 read sudo_passwd
 
-# # git config
-# # https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/credstores.md#gpgpass-compatible-files
-# dnf install -y pass git-credential-oauth
-# 
-# # shell install and config
-# dnf install -y zsh
-# git clone "$mygit/zsh-config" $zsh_config
-# 
-# # terminal install and config
-# dnf -y install alacritty tmux
-# mkdir -p $HOME/.config/alacritty/
-# git clone "$mygit/alacritty-config" $alacritty_config
-# 
+# git config
+# https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/credstores.md#gpgpass-compatible-files
+dnf install -y pass git-credential-oauth
+
+# shell install and config
+dnf install -y zsh
+git clone "$mygit/zsh-config" $zsh_config
+
+# terminal install and config
+dnf -y install alacritty tmux
+mkdir -p $HOME/.config/alacritty/
+git clone "$mygit/alacritty-config" $alacritty_config
+
 # i3 windows manager install and config
 # `arandr` - monitor config small app
-# echo $sudo_passwd | sudo -S yum install -y i3 arandr
-# 
-# # dev setup
-# dnf install -y neovim git make python3-pip npm cargo gcc-c++  
-# dnf copr enable atim/lazygit
-# dnf install lazygit
-# 
-# LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+# `lxappearance` - system font, theme, etc
+# `rofi` - alternative launcher
+# `i3blocks` - alternative bar
+echo $sudo_passwd | sudo -S yum install -y i3 arandr lxappearance rofi i3blocks
+
+# dev setup
+dnf install -y neovim git make python3-pip npm cargo gcc-c++  
+dnf copr enable atim/lazygit
+dnf install lazygit
+
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 # clone my config to lvim config dir. it is not empty, so do git init first
 pushd $config_dir/lvim
@@ -50,24 +52,24 @@ pushd $config_dir/lvim
   git reset origin/main  # Required when the versioned files existed in path before "git init" of this repo.
   git checkout -t origin/main -f
 popd
-# 
-# # nerd fonts (for lunarvim)
-# # Go mono font https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Go-Mono
-# wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Regular/GoMonoNerdFontMono-Regular.ttf
-# wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Bold/GoMonoNerdFontMono-Bold.ttf
-# wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Italic/GoMonoNerdFontMono-Italic.ttf
-# wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Bold-Italic/GoMonoNerdFontMono-BoldItalic.ttf
-# 
-# font_system_dir="/usr/share/fonts"
-# echo $sudo_passwd | sudo -S mkdir $font_system_dir/GoMonoNerdFontMono
-# echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-Regular.ttf $font_system_dir/GoMonoNerdFontMono
-# echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-Bold.ttf $font_system_dir/GoMonoNerdFontMono
-# echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-Italic.ttf $font_system_dir/GoMonoNerdFontMono
-# echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-BoldItalic.ttf $font_system_dir/GoMonoNerdFontMono
-# 
-# echo $sudo_passwd | sudo -S chown -R root: $font_system_dir/GoMonoNerdFontMono
-# echo $sudo_passwd | sudo -S chmod 644 $font_system_dir/GoMonoNerdFontMono/*
-# echo $sudo_passwd | sudo -S fc-cache -v
+
+# nerd fonts (for lunarvim)
+# Go mono font https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Go-Mono
+wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Regular/GoMonoNerdFontMono-Regular.ttf
+wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Bold/GoMonoNerdFontMono-Bold.ttf
+wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Italic/GoMonoNerdFontMono-Italic.ttf
+wget -P $home_dir/Downloads https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Go-Mono/Bold-Italic/GoMonoNerdFontMono-BoldItalic.ttf
+
+font_system_dir="/usr/share/fonts"
+echo $sudo_passwd | sudo -S mkdir $font_system_dir/GoMonoNerdFontMono
+echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-Regular.ttf $font_system_dir/GoMonoNerdFontMono
+echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-Bold.ttf $font_system_dir/GoMonoNerdFontMono
+echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-Italic.ttf $font_system_dir/GoMonoNerdFontMono
+echo $sudo_passwd | sudo -S cp $home_dir/Downloads/GoMonoNerdFontMono-BoldItalic.ttf $font_system_dir/GoMonoNerdFontMono
+
+echo $sudo_passwd | sudo -S chown -R root: $font_system_dir/GoMonoNerdFontMono
+echo $sudo_passwd | sudo -S chmod 644 $font_system_dir/GoMonoNerdFontMono/*
+echo $sudo_passwd | sudo -S fc-cache -v
 
 echo "FINISHED!"
 
